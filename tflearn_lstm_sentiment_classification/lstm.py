33cd21ec2117b1adf49e4f48458a77c2fa0e4286
郭同jet · 耐心 <779222056@qq.com>
2017-02-08 15:39:17 +0800

    Create lstm.py

---
+++ tflearn_lstm_sentiment_classification/lstm.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Apply an LSTM to IMDB sentiment dataset classification task
+import tflearn
+from tflearn.data_utils import to_categorical, pad_sequences
+from tflearn.datasets import imdb
+
+# IMDB Dataset loading 会自动下载
+train, test, _ = imdb.load_data(path='imdb.pkl', n_words=10000,
+                                valid_portion=0.1)
+trainX, trainY = train #22500个元素的list，每个元素类似这样[17,25,10,406,26,14,556,61,62,323,4]，可见是词在词库的位置
+testX, testY = test
+
+# Data preprocessing
+# Sequence padding
+trainX = pad_sequences(trainX, maxlen=100, value=0.)# 通过补零把list的每个元素长度都弄成100
+testX = pad_sequences(testX, maxlen=100, value=0.)
+# Converting labels to binary vectors
+trainY = to_categorical(trainY, nb_classes=2)#把0变成[0,1],把1变成[1,0]
+testY = to_categorical(testY, nb_classes=2)
+
+# Network building
+net = tflearn.input_data([None, 100])
+net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+net = tflearn.lstm(net, 128, dropout=0.8)
+net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
+                         loss='categorical_crossentropy')
+
+# Training
+model = tflearn.DNN(net, tensorboard_verbose=0)
+model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True,
+          batch_size=32)
